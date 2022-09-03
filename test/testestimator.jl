80dd43aad13633b97687edb183b0a50686196707
Mehmet Hakan Satman <mhsatman@gmail.com>
2022-09-03 16:09:12 +0300

    add tests for the eive-cga estimator

--- test/testestimator.jl
+++ test/testestimator.jl
@@ -38,8 +38,88 @@ end
     @test dist_clean_and_real < dist_dirty_and_real
     @test dist_eive_and_real < dist_dirty_and_real
 
-    @info "Clean OLS" cleanbetas
-    @info "Dirty OLS" dirtybetas
-    @info "CGA-EIVE Corrected " result.betas
+end
 
+
+
+
+@testset "Multiple regression (2 variables)" begin
+
+    rng = Random.MersenneTwister(1234)
+
+    n = 30
+
+    deltax = randn(rng, n) * sqrt(3.0)
+
+    cleanx = randn(rng, n) * sqrt(7.0)
+    cleanx2 = randn(rng, n) * sqrt(7.0)
+
+    e = randn(rng, n) * sqrt(5.0)
+
+    y = 20.0 .+ 10.0 .* cleanx .+ 15.0 .* cleanx2 .+ e
+
+    dirtyx = cleanx .+ deltax
+
+    Xd = hcat(ones(n), dirtyx, cleanx2)
+    Xc = hcat(ones(n), cleanx, cleanx2)
+    dirtybetas = Xd \ y
+    cleanbetas = Xc \ y
+
+    result = eive(dirtyx = dirtyx, y = y, otherx = cleanx2, popsize = 100, numdummies = 10)
+
+    dist_clean_and_real = euclidean(cleanbetas, [20.0, 10.0, 15.0])
+    dist_dirty_and_real = euclidean(dirtybetas, [20.0, 10.0, 15.0])
+    dist_eive_and_real = euclidean(result.betas, [20.0, 10.0, 15.0])
+
+    @test result isa EiveResult
+    @test dist_clean_and_real < dist_dirty_and_real
+    @test dist_eive_and_real < dist_dirty_and_real
+
+end
+
+
+
+
+@testset "Multiple regression (3 variables)" begin
+
+    rng = Random.MersenneTwister(1234)
+
+    n = 30
+
+    deltax = randn(rng, n) * sqrt(3.0)
+
+    cleanx = randn(rng, n) * sqrt(7.0)
+    cleanx2 = randn(rng, n) * sqrt(7.0)
+    cleanx3 = randn(rng, n) * sqrt(7.0)
+
+    e = randn(rng, n) * sqrt(5.0)
+
+    y = 20.0 .+ 10.0 .* cleanx .+ 15.0 .* cleanx2 .+ 13.0 .* cleanx3 .+ e
+
+    dirtyx = cleanx .+ deltax
+
+    Xd = hcat(ones(n), dirtyx, cleanx2, cleanx3)
+    Xc = hcat(ones(n), cleanx, cleanx2, cleanx3)
+    dirtybetas = Xd \ y
+    cleanbetas = Xc \ y
+
+    result = eive(
+        dirtyx = dirtyx,
+        y = y,
+        otherx = hcat(cleanx2, cleanx3),
+        popsize = 100,
+        numdummies = 10,
+    )
+
+    dist_clean_and_real = euclidean(cleanbetas, [20.0, 10.0, 15.0, 13.0])
+    dist_dirty_and_real = euclidean(dirtybetas, [20.0, 10.0, 15.0, 13.0])
+    dist_eive_and_real = euclidean(result.betas, [20.0, 10.0, 15.0, 13.0])
+
+    @test result isa EiveResult
+    @test dist_clean_and_real < dist_dirty_and_real
+    @test dist_eive_and_real < dist_dirty_and_real
+
+    #@info cleanbetas
+    #@info dirtybetas
+    #@info result
 end
