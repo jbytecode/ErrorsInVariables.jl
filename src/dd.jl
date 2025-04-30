90ee06ed07cd89f0e3a12f45ba24c48539735bdb
jbytecode <mhsatman@gmail.com>
2025-04-30 14:01:16 +0300

    add reference to dd estimator

--- src/dd.jl
+++ src/dd.jl
@@ -1,6 +1,16 @@
 module DD
 
+"""
 
+    dd(X::Matrix, y::Vector)::Matrix
+
+Computes the Higher Moment Estimator for a linear regression model with errors in the variables.
+
+# Reference
+
+- Dagenais, Marcel G., and Denyse L. Dagenais. "Higher moment estimators for linear regression models with errors in the variables." 
+Journal of Econometrics 76.1-2 (1997): 193-221.
+"""
 function dd(X::Matrix, y::Vector)::Matrix 
 
 end 
